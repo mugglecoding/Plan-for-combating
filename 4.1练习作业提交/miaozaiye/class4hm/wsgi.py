a0ffb9fdaec9d26a37dc06d4b34f17ecb2219445
Jiang Lu <fevberkeley@vip.sina.com>
2016-02-05 00:04:19 +0800

    4.1 homework
    4.1 homework

---
+++ 4.1练习作业提交/miaozaiye/class4hm/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for class4hm project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "class4hm.settings")
+
+application = get_wsgi_application()
