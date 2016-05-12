e20653a2a9716beced449377a66ae441cbe7bdae
Henson <shenghe@users.noreply.github.com>
2016-05-12 08:42:33 +0800

    Update main.py

--- week2/week2_homework/ganji/main.py
+++ week2/week2_homework/ganji/main.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
 from multiprocessing import Pool
-from page_parsing import get_item_info_from,url_list,item_info,get_links_from
+from page_parsing import get_item_info_from, url_list, item_info, get_links_from
 from channel_extracing import channel_list
 
 db_urls = [item['url'] for item in url_list.find()]
@@ -9,16 +12,12 @@ y = set(index_urls)
 rest_of_urls = x-y
 
 def get_all_links_from(channel):
-     for i in range(1,100):
-         get_links_from(channel,i)
+     for i in range(1, 100):
+         get_links_from(channel, i)
 
 
 if __name__ == '__main__':
     pool = Pool(processes=6)
-    # pool = Pool()
-    pool.map(get_all_links_from,channel_list.split())
+    pool.map(get_all_links_from, channel_list.split())
     pool.close()
     pool.join()
-
-
-
